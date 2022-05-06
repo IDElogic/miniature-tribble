4fd00d0ed8f91738edcf9dd8b772c21402482516
Ildiko CS Fabian <privat.fi@gmail.com>
2022-05-06 12:37:47 +0200

    DB mongoose

---
+++ api/routes/auth.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+   
+  });
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+module.exports = router;
